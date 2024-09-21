4731c109184a844ecfa9dbd19d253845a3708c4c
deborah <thomaskogolo@gmail.com>
2024-09-21 20:01:17 +0100

    OOP

---
+++ OOP.py
@@ -0,0 +1,18 @@
+class Human :
+
+ def __init__(self):
+     print( "\n   Hey I'm the constuctor,   \n     I help in creating object,   \n        When u create an object i am the first one to be called")
+
+
+
+
+
+
+deborah = Human()
+Akash = Human()
+Bello = Human()
+Mitchelle = Human()
+
+
+     
+    
