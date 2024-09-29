7368400c6903c7be9a259a411e35529777970024
deborah <thomaskogolo@gmail.com>
2024-09-29 21:25:55 +0100

    Birds.py

--- Birds.py
+++ Birds.py
@@ -1,1 +1,19 @@
-class = Human
+class Bird :
+  def __init__(self,species,maxAge):
+    self.species = species
+    self.maxAge = maxAge
+
+  def chirp (self):
+    print(self.name,"speaking")
+
+  def fly (self):
+    print(self.name,"flying")
+
+    Pigeon = Bird("Rock dove",15)
+    Parrot = Bird ("Kakapo",12)
+    Peacock = Bird("Green",20)
+
+
+    Parrot.chirp()
+    Pigeon.chirp()
+    Peacock.chirp()
