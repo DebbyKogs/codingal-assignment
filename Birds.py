08ad5b64d8b104cd963737d3d63485bd89c46477
deborah <thomaskogolo@gmail.com>
2024-10-05 18:55:36 +0100

    birds.py

--- Birds.py
+++ Birds.py
@@ -1,7 +1,8 @@
 class Bird :
-  def __init__(self,species,maxAge):
+  def __init__(self,species,maxAge,weight):
     self.species = species
     self.maxAge = maxAge
+    self.weight = weight
 
   def chirp (self):
     print(self.name,"speaking")
@@ -9,11 +10,30 @@ class Bird :
   def fly (self):
     print(self.name,"flying")
 
-    Pigeon = Bird("Rock dove",15)
-    Parrot = Bird ("Kakapo",12)
-    Peacock = Bird("Green",20)
+    Pigeon = Bird("Rock dove",15,"20kg")
+    Parrot = Bird ("Kakapo",12,"35kg")
+    Peacock = Bird("Green",20,"40kg")
 
 
     Parrot.chirp()
-    Pigeon.chirp()
+    Pigeon.fly()
     Peacock.chirp()
+
+
+    print("species of pigeon is", Pigeon.species)
+    print("maxAge of pigeon is", Pigeon.maxAge )
+    print("weight of pigeon is", Pigeon.weight)
+    
+
+
+    print("species of parrot is", Parrot.species)
+    print("maxAge of parrot is", Parrot.maxAge )
+    print("weight of parrot is", Parrot.weight)
+
+
+
+    print("species of Peacock is", Peacock.species)
+    print("maxAge of Peacock is", Peacock.maxAge )
+    print("weight of Peacock is", Peacock.weight)
+    
+    
