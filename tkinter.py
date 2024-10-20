6f88100198a2372cb8549f0ba93606fd2a4ccd6e
deborah <thomaskogolo@gmail.com>
2024-10-20 18:34:08 +0100

    tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -1,14 +1,14 @@
-import tkinter as TK
-root = tk.TK()
-root.title ("Tk intro")
+import tkinter as tk
+root = tk.Tk()
+root.title ("TK intro")
 root.geometry("500x500")
-textLabel = tk.Label(roottext="My name is Deborah")
+textLabel = tk.Label(root,text="My name is Deborah")
 textLabel.pack()
 entry = tk.Entry(root)
 entry.pack()
 def sayHelloFunction():
     x = entry.get()
     print("Hello!!!!!!!!!!!!!!!!",x)
-    sayHello = tk.Button(root,bg = "green",fg = "white",text = "sayHello!!!",command =sayHelloFunction, relief = "solid")
-    sayHello.pack()
-    root.mainloop()
+sayHello = tk.Button(root,bg = "green",fg = "white",text = "sayHello!!!",command =sayHelloFunction, relief = "solid")
+sayHello.pack()
+root.mainloop()
